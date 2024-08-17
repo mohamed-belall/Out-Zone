67d10c2247866805f5b3980c21e9aba6f5ca88c3
Mohammed Hassan Ahmed <123543218+MohammedHasanAhmed@users.noreply.github.com>
2024-08-17 16:36:26 +0300

    fix headerrrrr elbahermy

--- file.js
+++ file.js
@@ -3,4 +3,13 @@ function toggleMenu() {
     var menuToggle = document.querySelector('.menu-toggle');
     menu.classList.toggle('show');
     menuToggle.classList.toggle('active');
+
+    if (
+    menu.classList == 'show'
+    ) {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+
   }
