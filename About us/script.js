ee59d36339c0313c9638818c127f01eaaa2472e3
Moo ALaa <moalaa@gmail.com>
2024-08-20 21:30:15 +0300

    about us1

---
+++ About us/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const banner = document.querySelector(".banner");
+    let degree = 0;
+
+    setInterval(() => {
+        degree = (degree + 1) % 360;
+        banner.style.background = `conic-gradient(from ${degree}deg at bottom left, #051c6d 0%, #000cb7 20%, 
+            #005bd1 30%, #007a9b 40%, #4d056d 60%, #051c6d 71%)`;
+    }, 20); // Change degree every 100ms (10 times per second)
+});
