7f3e5df8f53caa54cc7eaa8e8d310ac0b45402f2
mohamed-belall <mohamedbelal.eng@gmail.com>
2024-08-21 16:39:36 +0300

    add project template

---
+++ projectTemplate/sidenav.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".sidenav .nav-link");
+  const contentSections = document.querySelectorAll(".content-section");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      // Remove 'active' class from all content sections and nav links
+      contentSections.forEach((section) => section.classList.remove("active"));
+      navLinks.forEach((nav) => nav.classList.remove("active"));
+
+      // Add 'active' class to the clicked link and corresponding content section
+      const contentId = this.getAttribute("data-content");
+      document.getElementById(contentId).classList.add("active");
+      this.classList.add("active");
+    });
+  });
+});
